1423bdf26896e7cf8287c2bba2f58c3fe4afee8b
AlexanderA24 <97633879+AlexanderA24@users.noreply.github.com>
2022-10-05 19:37:45 +0100

    commit

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,45 +1,66 @@
 import React from 'react';
+import axios from 'axios';
+
+import RegisterView from '../register-view/register-view';
+import LoginView from '../login-view/login-view';
 import MovieCard from '../movie-card/movie-card';
 import MovieView from '../movie-view/movie-view';
+
 
 class MainView extends React.Component {
 
     constructor() {
         super();
         this.state = {
-            movies: [
-                { _id: 1, Title: 'Inception', Description: 'A thief who steals corporate secrets through the use of dream-sharing technology is given the inverse task of planting an idea into the mind of a C.E.O., but his tragic past may doom the project and his team to disaste', ImagePath: 'C:/Users/Admin/Documents/GitHub/myFlix-client/img/inception.jpg' },
-                { _id: 2, Title: 'The Shawshank Redemption', Description: 'Andy Dufresne, a successful banker, is arrested for the murders of his wife and her lover, and is sentenced to life imprisonment at the Shawshank prison. He becomes the most unconventional prisoner.', ImagePath: 'img/ShawshankRedemptionMoviePoster.jpg' },
-                { _id: 3, Title: 'Gladiator', Description: 'Commodus takes over power and demotes Maximus, one of the preferred generals of his father, Emperor Marcus Aurelius. As a result, Maximus is relegated to fighting till death as a gladiator.', ImagePath: 'img\Gladiator.jpg' }
-            ],
-            selectedMovie: null
+            movies: [],
+            selectedMovie: null,
+            user: null
         }
     }
 
+    componentDidMount() {
+        axios.get('https://movie-api-aa.herokuapp.com/movies')
+            .then(response => {
+                this.setState({
+                    movies: response.data
+                });
+            })
+            .catch(error => {
+                console.log(error);
+            });
+    }
     setSelectedMovie(newSelectedMovie) {
         this.setState({
             selectedMovie: newSelectedMovie
         });
     }
 
+    onLoggedIn(user) {
+        this.setState({
+            user
+        });
+    }
+
     render() {
-        const { movies, selectedMovie } = this.state;
+        const { movies, selectedMovie, user } = this.state;
 
+        if (!user) return <RegisterView />;
 
-        if (movies.length === 0) return <div className="main-view">The list is empty!</div>;
+        /* If there is no user, the LoginView is rendered. If there is a user logged in, the user details are *passed as a prop to the LoginView*/
+        if (!user) return <LoginView onLoggedIn={user => this.onLoggedIn(user)} />;
+
+        if (movies.length === 0) return <div className="main-view" />;
 
         return (
             <div className="main-view">
                 {selectedMovie
                     ? <MovieView movie={selectedMovie} onBackClick={newSelectedMovie => { this.setSelectedMovie(newSelectedMovie); }} />
                     : movies.map(movie => (
-                        <MovieCard key={movie._id} movie={movie} onMovieClick={(movie) => { this.setSelectedMovie(movie) }} />
+                        <MovieCard key={movie._id} movie={movie} onMovieClick={(newSelectedMovie) => { this.setSelectedMovie(newSelectedMovie) }} />
                     ))
                 }
             </div>
         );
     }
-
 }
-
 export default MainView; //
